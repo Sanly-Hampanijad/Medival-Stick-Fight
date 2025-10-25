42fe3ffd7233d504fcd592ec1b0775b19f5abeb0
sanly hampanijad <sanly.hampanijad@icloud.com>
2025-10-25 13:57:17 +0100

    changes

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express = require('express');
 const { Socket } = require('node:dgram');
 const { createServer } = require('node:http');
 const { Server } = require('socket.io');
-
+const { Path } = require('path')
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "../index.html");
-});
+app.use(express.static('public'));
 
 io.on('connection', (socket) => {
     console.log('a user has connected');
